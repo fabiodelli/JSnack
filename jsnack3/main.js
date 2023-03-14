505fb1ef558462c1406fa79436acb69656e4d877
fabiodelli <ntqdel@hotmail.com>
2023-03-14 12:47:56 +0100

    Stampa riusltato solo alla fine

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -3,5 +3,6 @@ let tot= 0
 for(let i = 1; i <= 10; i++ ){
     const insertNumber = Number(prompt("Inserisci un numero"))
     tot = tot + insertNumber
-    console.log(tot)
+    
 }
+console.log(tot)
