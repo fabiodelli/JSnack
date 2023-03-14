648f29164dad26c587142fd7b73b62ffe7c78826
fabiodelli <ntqdel@hotmail.com>
2023-03-14 12:49:23 +0100

    better indentation

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -2,7 +2,7 @@ let tot= 0
 
 for(let i = 1; i <= 10; i++ ){
     const insertNumber = Number(prompt("Inserisci un numero"))
-    tot = tot + insertNumber
-    
+    tot = tot + insertNumber 
 }
+
 console.log(tot)
