e4f21a780f157f9eb27a63b8c4fead2bc6b3df6e
fabiodelli <ntqdel@hotmail.com>
2023-03-14 12:16:34 +0100

    add space

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -5,9 +5,9 @@ const secondWord = prompt("inserisci una parola")
 /* console.log(secondNumber) */
 
 if (firstWord.length > secondWord.length){
-    console.log(`${secondWord}${firstWord} la prima parola è piu lunga`)}
+    console.log(`${secondWord} ${firstWord} la prima parola è piu lunga`)}
     else if (secondWord.length > firstWord.length) {
-        console.log(`${firstWord}${secondWord} la seconda parola è piu lunga`)
+        console.log(`${firstWord} ${secondWord} la seconda parola è piu lunga`)
     }
     else {
         console.log(`le parole hanno la solita lunghezza`)
