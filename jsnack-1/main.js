ab27f538bc36dd8e0f0eb8d6aa5aae5eb8e40db5
fabiodelli <ntqdel@hotmail.com>
2023-03-14 11:48:07 +0100

    snack-1

---
+++ jsnack-1/main.js
@@ -0,0 +1,11 @@
+const firstNumber = prompt("inserisci un numero")
+console.log(firstNumber)
+
+const secondNumber = prompt("inserisci un numero")
+console.log(secondNumber)
+
+if (firstNumber > secondNumber){
+    console.log(firstNumber)}
+    else{
+        console.log(secondNumber)
+    }
