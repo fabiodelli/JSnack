01334e5cebaacfb053218f9b525574a58b6beb03
fabiodelli <ntqdel@hotmail.com>
2023-03-15 11:52:51 +0100

    while cicle

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,8 +1,18 @@
+let i = 1
 let tot= 0
 
-for(let i = 1; i <= 10; i++ ){
+/* for(let i = 1; i <= 10; i++ ){
     const insertNumber = Number(prompt("Inserisci un numero"))
     tot = tot + insertNumber 
 }
 
+console.log(tot) */
+
+
+while (i <= 10) {
+   const insertNumber = Number(prompt("Inserisci un numero"))
+    tot = tot + insertNumber 
+
+    i++
+}
 console.log(tot)
