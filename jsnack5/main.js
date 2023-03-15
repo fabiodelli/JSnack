b82f5da2c70c85ffbf2f9e095644d13360d482e5
fabiodelli <ntqdel@hotmail.com>
2023-03-15 14:59:59 +0100

    completato fuori tempo limite

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -1,11 +1,22 @@
 
 const emptyArray=[]
 
-for(let i = 0 ; i < 6 ; i++ ){
+/* for(let i = 0 ; i < 6 ; i++ ){
     const insertNumber = Number(prompt("Inserisci un numero"))
     
     if(insertNumber % 2 !== 0){
         emptyArray.push(insertNumber)
     }
-    console.log(emptyArray)
 }
+console.log(emptyArray) */
+
+let i = 0
+
+while(i < 6){
+    const insertNumber = Number(prompt("Inserisci un numero"))
+    if(insertNumber % 2 !== 0){
+        emptyArray.push(insertNumber)    
+} 
+i++
+}
+console.log(emptyArray)
